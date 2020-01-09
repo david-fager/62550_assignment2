88a6ff26bd8c02dbfe9f10eeffa2d484bbc4bc17
DAFA <s185120@student.dtu.dk>
2020-01-09 15:51:39 +0100

    Loading ikon tilføjet

--- app/src/androidTest/java/com/dtu/s185120_62550_Galgeleg2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dtu/s185120_62550_Galgeleg2/ExampleInstrumentedTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented element_wordlist_text, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under element_wordlist_text.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.dtu.s185120_62550_Galgeleg2", appContext.getPackageName());
--- app/src/main/java/com/dtu/s185120_62550_Galgeleg2/Frag_wordlist.java
+++ app/src/main/java/com/dtu/s185120_62550_Galgeleg2/Frag_wordlist.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -19,9 +21,11 @@ public class Frag_wordlist extends Fragment implements View.OnClickListener {
 
     private Galgelogik tempGalgelogik;
     private ArrayList accessible_words, dr_words;
+    private ConstraintLayout loading;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View popup = inflater.inflate(R.layout.frag_wordlist, container, false);
+        loading = popup.findViewById(R.id.wordlist_layout_progress);
 
         tempGalgelogik = new Galgelogik();
         accessible_words = tempGalgelogik.muligeOrd;
@@ -29,7 +33,7 @@ public class Frag_wordlist extends Fragment implements View.OnClickListener {
         // Tries to get all words from DR so the user can choose on of those
         new AsyncTask<String, Void, Void>() {
             @Override
-            protected void onPreExecute() {}
+            protected void onPreExecute() {loading.setVisibility(View.VISIBLE);}
 
             @Override
             protected Void doInBackground(String... strings) {
@@ -45,6 +49,7 @@ public class Frag_wordlist extends Fragment implements View.OnClickListener {
             protected void onPostExecute(Void aVoid) {
                 dr_words = tempGalgelogik.muligeOrd;
                 tempGalgelogik = null; // Making the object into garbage for cleaning up.
+                loading.setVisibility(View.INVISIBLE);
                 setupListview();
             }
         }.execute();
@@ -63,11 +68,11 @@ public class Frag_wordlist extends Fragment implements View.OnClickListener {
 
         // Actually making the list of the words
         ListView listView = getActivity().findViewById(R.id.wordlist_listview);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.test, derp) {
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.element_wordlist_text, derp) {
             @NonNull
             @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                View view = getActivity().getLayoutInflater().inflate(R.layout.test, null, true);
+                View view = getActivity().getLayoutInflater().inflate(R.layout.element_wordlist_text, null, true);
                 TextView asd = view.findViewById(R.id.listview_element);
                 asd.setText(derp[position]);
                 return view;
